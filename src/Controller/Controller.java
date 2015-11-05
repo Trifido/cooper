65529a5d646c43ba76b1ce5b08b54a5d8acca770
Alba <little.sheena.x7@gmail.com>
2015-11-05 23:02:29 +0100

    Estructura de datos de Controller

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controller;
 
 import es.upv.dsic.gti_ia.core.AgentID;
@@ -10,12 +5,33 @@ import es.upv.dsic.gti_ia.core.SingleAgent;
 
 /**
  *
- * @author mipc
+ * Clase Controller
+ * Recibe mensajes del agente Listener y realiza la toma de decisiones
+ * 
+ * @author Alba Ríos
  */
 public class Controller extends SingleAgent{
     
+    private int battery;
+    private Pair<Integer, Integer> gps;
+    private int[][] radar;
+    private double[][] scanner;
+    private Square[][] world;
+    
     public Controller(AgentID aid) throws Exception {
         super(aid);
+        this.battery = 0;
+        this.gps = new Pair(0,0);
+        this.radar = new int[5][5];
+        this.scanner = new double[5][5];
+        this.world = new Square[500][500];
+        
+        // Inicializar el mapa completo a valor "desconocido"
+        for (Square[] row : this.world){
+            for (Square s : row){
+                s = Square.UNKNOWN;
+            }
+        }
     }
     
     @Override
---
+++ src/Controller/Square.java
@@ -0,0 +1,12 @@
+package Controller;
+
+/**
+ *
+ * Enum Square
+ * Describe el tipo de casilla que puede existir en el mapa del mundo del agente
+ * 
+ * @author Alba Ríos
+ */
+public enum Square {
+    EMPTY, OBSTACLE, GOAL, UNKNOWN
+}
