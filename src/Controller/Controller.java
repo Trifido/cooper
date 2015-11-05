acb6b84fc7a65c50dc823ac2b573835dbd4c988a
Alba <little.sheena.x7@gmail.com>
2015-11-05 21:48:01 +0100

    Clase controller

---
+++ src/Controller/Controller.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import es.upv.dsic.gti_ia.core.AgentID;
+import es.upv.dsic.gti_ia.core.SingleAgent;
+
+/**
+ *
+ * @author mipc
+ */
+public class Controller extends SingleAgent{
+    
+    public Controller(AgentID aid) throws Exception {
+        super(aid);
+    }
+    
+    @Override
+    public void execute(){
+    }
+    
+}
