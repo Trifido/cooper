8b9c530a3519c047b3bc31652aa65e7ce5b81c5a
Rafaeling <rafaelingugr@gmail.com>
2015-11-06 12:55:03 +0100

    Función sendAction para enviar nueva posición al servidor (No acabada! Falta la KEY)

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -160,9 +160,21 @@ public class Controller extends SingleAgent{
      * @author Rafael Ruiz
      * @param localization Nueva posición para el agente
      */
-    public void sendLocalization(String localization)
+    public void sendAction(String action)
     {
         
+        // Composición del Json de sendLocalization
+        this.msg = Json.object().add( "command", action );
+        this.msg.add( "key", this.key.get( "key" ).asString() );
+        
+        // Creación del ACL
+        this.out = new ACLMessage();
+        this.out.setSender( this.getAid() );
+        this.out.setReceiver( new AgentID( "Furud" ) );
+        
+        this.out.setContent( this.msg.toString() );
+        
+        this.send( out );
     }
     
     @Override
